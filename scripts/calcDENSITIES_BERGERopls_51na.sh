f8697db518ef530a36496de6a805ce8ba72b0589
ohsOllila <samuli.ollila@aalto.fi>
2015-11-25 17:12:12 +0200

    New order in density plot

--- scripts/calcDENSITIES_BERGERopls_51na.sh
+++ scripts/calcDENSITIES_BERGERopls_51na.sh
@@ -12,7 +12,7 @@ CAindexNR=$(echo q | make_ndx -f $tprname | grep " NA" | awk '{if(NR==1)print $1
 CLindexNR=$(echo q | make_ndx -f $tprname | grep " CL" | awk '{if(NR==1)print $1}')
 echo $LIPIDindexNR System | trjconv -f $trajname -s $tprname -fit progressive -o FITtraj.xtc
 #Ztrans=$(tail -n 1 $groFILE | awk '{print $3/2}')
-Ztrans=-1
+Ztrans=-1.25
 echo System | trjconv -f FITtraj.xtc -s $tprname -trans 0 0 $Ztrans -o ANALtraj.xtc
 echo $LIPIDindexNR $CAindexNR $CLindexNR | g_density -f ANALtraj.xtc -s $tprname -dens number -o densNC.xvg -xvg none -sl 100 -ng 3
 echo System | trjconv -f ANALtraj.xtc -s $tprname -o INBOXtraj.xtc -pbc mol
