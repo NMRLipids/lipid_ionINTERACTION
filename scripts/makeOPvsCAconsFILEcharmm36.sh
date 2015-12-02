fec6c801daaf693b1200e1e30799523a1bea0d7c
ohsOllila <samuli.ollila@aalto.fi>
2015-12-02 17:49:39 +0200

    Add CHARMM36 CaCl_2 results and citations to table

--- scripts/makeOPvsCAconsFILEcharmm36.sh
+++ scripts/makeOPvsCAconsFILEcharmm36.sh
@@ -3,6 +3,10 @@ rm $outfile
 echo "     b1           SD        b2          SD         a1          SD       a2        SD       g11        SD       g12        SD       g2        SD       g31        SD       g32        SD" >> ../Data/POPC/CaCl/CHARMM36/OPvsCACLcons.dat
 paste -sd " " ../Data/POPC/NaCl/CHARMM36/0na/OrderParameters.dat > tmp.dat 
 awk '{print "0 "$0}' tmp.dat >> $outfile
+paste -sd " " ../Data/POPC/CaCl/CHARMM36/35ca/OrderParameters_last100ns.dat > tmp.dat 
+awk '{print "350 "$0}' tmp.dat >> $outfile
 paste -sd " " ../Data/POPC/CaCl/CHARMM36/26ca/OrderParameters.dat > tmp.dat 
 awk '{print "670 "$0}' tmp.dat >> $outfile
+paste -sd " " ../Data/POPC/CaCl/CHARMM36/100ca/OrderParameters_last100ns.dat > tmp.dat 
+awk '{print "1000 "$0}' tmp.dat >> $outfile
 rm tmp.dat
