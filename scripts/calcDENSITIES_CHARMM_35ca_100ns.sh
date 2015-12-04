e0c1e43318e26e180f37b14a51e9144802dbf307
MykhailoGirych <girychms@gmail.com>
2015-12-04 11:58:07 +0200

    035M_1M_CaCl2_CRM36_POPC_density_profiles

--- scripts/calcDENSITIES_CHARMM_35ca_100ns.sh
+++ scripts/calcDENSITIES_CHARMM_35ca_100ns.sh
@@ -4,7 +4,7 @@ mkdir $tmpDIRname
 cd $tmpDIRname
 trajname=/work/girychm1/24_11_15/035m_popc_crm/gromacs/035m_popc_crm_200ns.xtc
 tprname=/work/girychm1/24_11_15/035m_popc_crm/gromacs/200ns_run.tpr
-mappingFILE=../../MAPPING/mappingPOPClipid14.txt
+mappingFILE=../../MAPPING/mappingPOPCcharmm.txt
 outFILE=../../Data/POPC/CaCl/CHARMM36/35ca/LIPIDdensity_100ns.xvg
 groFILE=/work/girychm1/24_11_15/035m_popc_crm/gromacs/confout.gro
 #trjcat -f popcCHOL50molPER0-25ns.trr popcCHOL50molPER25-50ns.trr
