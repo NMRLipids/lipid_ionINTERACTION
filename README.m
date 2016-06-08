1127d392c0c081668cbb8c578a29d2a2517f46ce
Samuli <samuli.ollila@aalto.fi>
2016-06-08 10:01:42 +0300

    Add readme

---
+++ README.m
@@ -0,0 +1,13 @@
+This is the repository of NMRlipids II project.
+This is part of the Open Collaboration project
+running at nmrlipids.blogspot.fi to understand 
+lipid structures with atomistic detail.
+
+[Manuscript] (https://github.com/NMRLipids/lipid_ionINTERACTION/tree/master/Manuscript)  -- contains the current version of the manuscript in raw LaTex format and in pdf.
+	      [Data] (https://github.com/NMRLipids/lipid_ionINTERACTION/tree/master/Data) -- contains the data used in the manuscript
+	      [Figs] (https://github.com/NMRLipids/lipid_ionINTERACTION/tree/master/Fig) -- contains the figures and scripts to generate those from the Data folder.
+	      [Scratch] (https://github.com/NMRLipids/lipid_ionINTERACTION/tree/master/scratch) -- contains everything else and all the work in progress which is not ready for the Data folder.
+
+The up-to-date manuscript of each project will be always found from the Manuscript folder. The manuscript also contains a ToDo list with specific issues to be improved (unless it is finished). The discussion will be done at GitHub issue tracker and at [nmrlipids.blogspot.fi](nmrlipids.blogspot.fi).
+
+  The [NMRlipids authorship guidelines](http://nmrlipids.blogspot.fi/2013/07/on-credits.html) apply to this repository.
